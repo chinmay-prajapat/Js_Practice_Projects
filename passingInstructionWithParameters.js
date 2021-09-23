39e106df5ef47205c800cb11d970e8041c385918
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-09-23 12:22:21 +0530

    Create passingInstructionWithParameters.js
    instructions passed to another function as an argument

---
+++ passingInstructionWithParameters.js
@@ -0,0 +1,10 @@
+ pushValueToArray=function(array,multiply){
+const output=[]
+array.map(i=>output.push(multiply(i)))
+return output;
+}
+let multiply=function(input){
+return input*2;
+}
+let arr=[1,2,3]
+let result=pushValueToArray(arr,multiply)
