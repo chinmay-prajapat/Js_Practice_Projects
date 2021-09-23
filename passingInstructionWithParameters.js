b9abd8d76cd5a4bb710e10cbebca90635386c9c5
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-09-23 12:27:20 +0530

    Update passingInstructionWithParameters.js
    passed arrow function

--- passingInstructionWithParameters.js
+++ passingInstructionWithParameters.js
@@ -3,8 +3,8 @@ const output=[]
 array.map(i=>output.push(multiply(i)))
 return output;
 }
-let multiply=function(input){
-return input*2;
-}
+// let multiply=function(input){
+// return input*2;
+// }
 let arr=[1,2,3]
-let result=pushValueToArray(arr,multiply)
+let result=pushValueToArray(arr,input=>input*2)//this is an arrow function
